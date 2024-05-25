8644e44a7566daa8905e677fadc3af70f31c3cfc
Thania Cruz <tmeritzel.cb@gmail.com>
2024-05-25 02:22:29 -0600

    agregando los eventos onclick y onchange

--- src/Components/Search/TodoSearch.js
+++ src/Components/Search/TodoSearch.js
@@ -2,7 +2,16 @@ import './TodoSearch.css'
 
 function TodoSearch(){
   return(
-    <input placeholder="Cortar cebolla"  className="TodoSearch"/>
+    <input 
+    placeholder="Cortar cebolla"  
+    className="TodoSearch"
+    onChange={(event)=>{
+      console.log('Escribiste en el TodoSearch');
+      console.log(event);
+      console.log(event.target);
+      console.log(event.target.value);
+    }}
+    />
   );
 }
 
